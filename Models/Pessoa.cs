ccbc87f4442f49dee908c2420a159816ffe199c5
Micael Souza <micaelbatista0108@hotmail.com>
2023-12-25 19:46:36 -0300

    tipos de variáveis

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+//Caminho lógico != Caminho físico
 namespace exemploFundamentos.Models
 {
     public class Pessoa
     {
-        public required string Nome { get; set; }
-        public required int Idade { get; set; }
+        public string? Nome { get; set; }
+        public int? Idade { get; set; }
 
         //Classe -> Pessoa
         //Método -> Apresentar
@@ -17,7 +18,13 @@ namespace exemploFundamentos.Models
         {
             //Classe -> Console
             //Método -> WriteLine
-            Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+            //Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
+
+            //"Quebrando" o camando
+            Console.WriteLine($"Olá, meu nome é {Nome}, e " +
+            $"tenho {Idade} anos");
+
+            //Para quebrar a linha usar o /n
         }
         
     }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using exemploFundamentos.Models;
+
+//classe e variável
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Teste";
+p1.Idade = 2;
+p1.Apresentar();
+
+string apresentacao = "Olá, seja bem vindo";
+
+int quantidade = 1;
+
+double altura = 1.80;
+
+decimal preco = 1.80M;
+
+bool condicao = true;
+
+DateTime dataAtual = DateTime.Now.AddDays(5);
+
+Console.WriteLine(apresentacao);
+Console.WriteLine(quantidade);
+Console.WriteLine(altura);
+Console.WriteLine(altura.ToString("0.00"));
+Console.WriteLine(preco);
+Console.WriteLine(condicao);
+Console.WriteLine(dataAtual);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy"));
